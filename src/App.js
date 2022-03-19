2cb4650207aa0d926f894d2e472e6e8d2b1cdcad
Shivansh Shukla <shuklashivansh@adaptideations.com>
2022-03-19 19:41:45 +0530

    remove app.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Products } from "./pages/products/Products";
 import { Login } from "./pages/Login/Login";
